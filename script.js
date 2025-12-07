3d37b1b0dfa29483fdb022fef6559423dfa727df
Joel-Tuff <megamcher@gmail.com>
2025-12-07 18:17:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // ✅ HIER kannst du deine Login-Daten ändern:
-let correctUsername = "admin";
+let correctUsername = "katja";
 let correctPassword = "1234";
 
 // ✅ HIER kannst du den Fehlertext ändern:
-let errorMessage = "TIPP: Benutzer=Name Passwort=Datum ";
+let errorMessage = "Falscher Benutzername oder Passwort!";
 
 function login() {
     let usernameInput = document.getElementById("username").value;
     let passwordInput = document.getElementById("password").value;
     let errorText = document.getElementById("error-text");
 
+    let loginBox = document.getElementById("loginBox");
+    let welcomeScreen = document.getElementById("welcomeScreen");
+    let welcomeText = document.getElementById("welcomeText");
+
     if (usernameInput === correctUsername && passwordInput === correctPassword) {
-        errorText.style.color = "lime";
-        errorText.textContent = "Login erfolgreich!";
-        
-        // Optional: Weiterleitung
-        // window.location.href = "dashboard.html";
+        // ✅ LOGIN AUSBLENDEN
+        loginBox.style.display = "none";
+
+        // ✅ WILLKOMMEN TEXT SETZEN
+        welcomeText.textContent = "Willkommen " + usernameInput.toUpperCase();
+
+        // ✅ ANIMATION EINBLENDEN
+        welcomeScreen.style.display = "flex";
+
     } else {
         errorText.style.color = "#ff4d4d";
         errorText.textContent = errorMessage;
     }
 }
-
