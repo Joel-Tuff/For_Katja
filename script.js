5095d27f0a417a511ccb9b1336a01e6f9474e769
Joel-Tuff <megamcher@gmail.com>
2025-12-07 17:31:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function showMessage() {
+    const msg = document.getElementById("message");
+    msg.innerText = "You clicked the button, Katja 😎";
+    msg.style.marginTop = "20px";
+    msg.style.fontSize = "1.2rem";
+    msg.style.color = "#a855f7";
+    msg.style.textShadow = "0 0 10px #a855f7";
+}
 
